7922f91dd050fe5c662259ffd7a3da7476c8bb86
mateuszmagier <mateuszmagier@bitbucket.org>
2018-09-22 13:43:42 +0200

    Add quotes section with quotes slider and laptop styling.

---
+++ js/script.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const QuotesSlider = function (quotesContainerSelector) {
+        this.container = document.querySelector(quotesContainerSelector);
+        this.quotes = this.container.querySelectorAll(".quote");
+        this.activeQuote = this.container.querySelector(".quote-active");
+        this.leftArrow = this.container.querySelector(".left-arrow");
+        this.rightArrow = this.container.querySelector(".right-arrow");
+
+        this.onArrowClick = function (isLeft) {
+            let newActiveQuote = null;
+
+            if (isLeft) { // next quote
+                if (this.activeQuote.nextElementSibling.classList.contains("quote"))
+                    newActiveQuote = this.activeQuote.nextElementSibling;
+                else
+                    newActiveQuote = this.quotes[0];
+            } else { // previous quote
+                if (this.activeQuote.previousElementSibling.classList.contains("quote"))
+                    newActiveQuote = this.activeQuote.previousElementSibling;
+                else
+                    newActiveQuote = this.quotes[this.quotes.length - 1];
+            }
+
+            this.activeQuote.classList.remove("quote-active");
+            setTimeout(function () {
+                this.activeQuote.classList.add("quote-invisible");
+                newActiveQuote.classList.remove("quote-invisible");
+                newActiveQuote.classList.add("quote-active");
+                this.activeQuote = newActiveQuote;
+            }.bind(this), 300);
+        }
+
+        this.registerArrowsClickListeners = function () {
+
+            this.leftArrow.addEventListener("click", function () {
+                this.onArrowClick(true);
+            }.bind(this));
+
+            this.rightArrow.addEventListener("click", function () {
+                this.onArrowClick(false);
+            }.bind(this));
+        };
+
+        this.init = function () {
+            this.registerArrowsClickListeners();
+        };
+    };
+
+
+
+    let slider = new QuotesSlider(".quotes-container");
+    slider.init();
+
+});
